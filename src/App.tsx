c04bc285294a91510b0791d201d078725fa61e35
Lewis Alderman <lewisalderman95@gmail.com>
2024-07-13 15:36:24 +0100

    Persisted favouriting

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,31 @@ import {useEffect, useState} from "react";
 import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "./components/ui/card";
 import dayjs from "dayjs";
 import RelativeTime from "dayjs/plugin/relativeTime";
+import {Button} from "./components/ui/button";
+import {cn} from "./lib/utils";
+import {create} from 'zustand';
+import {devtools, persist} from 'zustand/middleware';
+
+interface FavouritesState {
+  ids: number[]
+  clear: () => void
+  toggle: (id: number) => void
+}
+
+const useFavouritesStore = create<FavouritesState>()(
+  devtools(
+    persist(
+      (set) => ({
+        ids: [],
+        clear: () => set(() => ({ids: []})),
+        toggle: (id) => set((state) => ({ids: state.ids.includes(id) ? state.ids.filter((i) => i !== id) : [...state.ids, id]})),
+      }),
+      {
+        name: 'favourite-storage',
+      },
+    ),
+  ),
+)
 
 dayjs.extend(RelativeTime);
 
@@ -30,6 +55,9 @@ const getGhRepos = async () => {
 function App() {
   const [searchData, setSearchData] = useState<Awaited<ReturnType<typeof getGhRepos>> | null>(null);
 
+  const favourites = useFavouritesStore((state) => state.ids);
+  const toggleFavourite = useFavouritesStore((state) => state.toggle);
+
   useEffect(() => {
     (async () => {
       console.log('fetching data');
@@ -40,63 +68,75 @@ function App() {
 
   return (
     <ul className="list-none flex flex-wrap *:basis-[350px] gap-5">
-      {searchData?.items.map((item) => (
-        <li key={item.id} className="flex flex-col">
-          <Card className="flex-1 flex flex-col">
-            <CardHeader>
+      {searchData?.items.map((item) => {
+        const isFavourite = favourites.includes(item.id);
 
-              {/* Repo title */}
-              <CardTitle className="leading-snug break-all text-balance">
-                {item.owner?.login}
-                {' '}/{' '}
-                <a href={item.html_url} target="_blank" className="group inline-block relative hover:text-blue-500">
-                  {item.name}
-                  <span className="opacity-0 absolute top-1/2 -translate-y-1/2 group-hover:opacity-50 text-xs  pl-2">⤴</span>
-                </a>
-              </CardTitle>
+        return (
+          <li key={item.id} className="flex flex-col">
+            <Card className="flex-1 flex flex-col">
+              <CardHeader>
+                <div className="flex">
+                  {/* Repo title */}
+                  <CardTitle className="grow leading-snug break-all text-balance -mt-[0.2lh]">
+                    {item.owner?.login}
+                    {' '}/{' '}
+                    <a href={item.html_url} target="_blank" className="group inline-block relative hover:text-blue-500">
+                      {item.name}
+                      <span className="opacity-0 absolute top-1/2 -translate-y-1/2 group-hover:opacity-50 text-xs  pl-2">⤴</span>
+                    </a>
+                  </CardTitle>
 
-              {/* Time ago */}
-              <CardDescription>
-                <span className="sr-only">Created</span> {dayjs(item.created_at).fromNow()}
-              </CardDescription>
-
-            </CardHeader>
-
-            {/* Description */}
-            <CardContent className="flex-1">
-              <p className="line-clamp-3 min-h-[3lh]" title={item.description ?? ""}>
-                {item.description ?? "\u00A0" /*&nbsp;*/}
-              </p>
-            </CardContent>
-
-            <CardFooter>
-              <CardDescription>
-                <ul className="flex gap-x-2 items-baseline [&>*:not(:last-child)]:after:content-['·'] [&>*:not(:last-child)]:after:ml-2 [&>*:not(:last-child)]:after:opacity-30">
-                  {/* Stars */}
-                  <li>
-                    <span aria-hidden className="leading-[20px] select-none align-top opacity-50 mr-1 font-light">
-                      ☆
+                  <Button variant={"ghost"} className={cn("-mr-3 text-xl", isFavourite ? "text-orange-400" : "opacity-30")} onClick={() => toggleFavourite(item.id)}>
+                    <span aria-hidden>
+                      {isFavourite ? "★" : "☆"}
                     </span>
+                    <span className="sr-only">Toggle favourite</span>
+                  </Button>
+                </div>
 
-                    {item.stargazers_count} <span className="sr-only">stars</span>
-                  </li>
+                {/* Time ago */}
+                <CardDescription>
+                  <span className="sr-only">Created</span> {dayjs(item.created_at).fromNow()}
+                </CardDescription>
 
-                  {/* Lang */}
-                  {item.language && (
+              </CardHeader>
+
+              {/* Description */}
+              <CardContent className="flex-1">
+                <p className="line-clamp-3 min-h-[3lh]" title={item.description ?? ""}>
+                  {item.description ?? "\u00A0" /*&nbsp;*/}
+                </p>
+              </CardContent>
+
+              <CardFooter>
+                <CardDescription>
+                  <ul className="flex gap-x-2 items-baseline [&>*:not(:last-child)]:after:content-['·'] [&>*:not(:last-child)]:after:ml-2 [&>*:not(:last-child)]:after:opacity-30">
+                    {/* Stars */}
                     <li>
-                      <span aria-hidden className="text-xs leading-[20px] select-none align-top opacity-50 mr-1">{`{ }`}</span>
+                      <span aria-hidden className="leading-[20px] select-none align-top opacity-50 mr-1 font-light">
+                        ☆
+                      </span>
 
-                      <span className="sr-only">Written in</span>
-
-                      {item.language}
+                      {item.stargazers_count} <span className="sr-only">stars</span>
                     </li>
-                  )}
-                </ul>
-              </CardDescription>
-            </CardFooter>
-          </Card>
-        </li>
-      ))}
+
+                    {/* Lang */}
+                    {item.language && (
+                      <li>
+                        <span aria-hidden className="text-xs leading-[20px] select-none align-top opacity-50 mr-1">{`{ }`}</span>
+
+                        <span className="sr-only">Written in</span>
+
+                        {item.language}
+                      </li>
+                    )}
+                  </ul>
+                </CardDescription>
+              </CardFooter>
+            </Card>
+          </li>
+        )
+      })}
     </ul>
   )
 }
